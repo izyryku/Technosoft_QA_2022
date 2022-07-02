f3b6668b45a1a62b2a2d1bb8b77dd826799f53a7
Igli Zyryrku <iglizyryrku@Iglis-MacBook-Pro.local>
2022-07-01 21:29:20 -0400

    My first commit

---
+++ FirstPackage/firstCode.js
@@ -0,0 +1,22 @@
+console.log('welcome Igli to coding!')
+let lVar1=10;
+let lVar2=20;
+let lVar3=30;
+
+let res1 = lVar1 + lVar2++
+console.log(`\n\nlVar1 = ${lVar1}`);
+
+
+let teamSize = 7;
+teamSize++;
+let teamName = `Warriors`;
+console.log(`Our team name is ${teamName} our team size is ${teamSize}`)
+
+let age = 32;
+age++;
+console.log(`Since today is my birthday, I am ${age} years old`);
+
+let accountBalance = 1000;
+accountBalance--;
+console.log(`I spent a dollar to buy a water and now my new balance is ${accountBalance}`);
+
